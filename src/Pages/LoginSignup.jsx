4a06d3af265e5167421dbdd3538292c045b930e8
SARATHKUMAR0412 <sarath0412s@gmail.com>
2024-02-15 10:00:45 +0530

    Fix: change some code

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CSS/LoginSignup.css";
 import { Link } from "react-router-dom";
 
 const LoginSignup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="loginsignup">
       <div className="loginsignup-container">
         <h1>Sign Up</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder="Enter  your name" />
-          <input type="email" placeholder="Enter your email" />
-          <input type="password" placeholder="Enter your password" />
+          {/* <input type="text" placeholder="Enter  your name" /> */}
+          <input type="email" placeholder="Enter your email" value={email} />
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+          />
         </div>
         <button>Continue</button>
         <p className="loginsignup-login">

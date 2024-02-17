63fe7f952dbe780f3b6574447f959f35fed02867
SARATHKUMAR0412 <sarath0412s@gmail.com>
2024-02-17 19:45:41 +0530

    Fix:login files changed to auth folder

---
+++ src/Pages/Auth/LoginSignup.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "./CSS/LoginSignup.css";
+import { Link } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+
+const LoginSignup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <form onSubmit={signIn}>
+          <h1>Create Account</h1>
+          <div className="loginsignup-fields">
+            {/* <input type="text" placeholder="Enter  your name" /> */}
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Signup</button>
+          <p className="loginsignup-login">
+            Already have an account?
+            <Link to="/login">
+              <span>Login here</span>
+            </Link>
+          </p>
+
+          <div className="loginsignup-agree">
+            <input type="checkbox" name="" id="" />
+            <p>By continuing, i agree to the terms of use & privacy policy.</p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignup;
